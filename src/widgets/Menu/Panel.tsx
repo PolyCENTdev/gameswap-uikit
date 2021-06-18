2340a33bdf9ee26d84df3d91a489ebf729e22f94
PolyCENTdev <85699318+PolyCENTdev@users.noreply.github.com>
2021-06-18 22:56:49 +0900

    Update Panel.tsx

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -39,7 +39,6 @@ const Panel: React.FC<Props> = (props) => {
   return (
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
       <PanelBody {...props} />
-      <PanelFooter {...props} />
     </StyledPanel>
   );
 };
